454a3c876378d4eb92ff2ba1e3fbdb58e11eb3f9
Анастасия Ельшина <salnaya92@gmail.com>
2023-04-13 15:12:30 +0300

    Project successfully done

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,6 +1,26 @@
 export default class Team {
-  constructor(team) {
-    this.team = [team];
-    [Symbol.iterator] = [][Symbol.iterator];
+  constructor() {
+    this.team = new Set();
+  }
+
+  * [Symbol.iterator]() {
+    let index = 0;
+    const entries = Object.entries(this.team);
+    while (index <= entries.length) {
+      yield entries[index];
+      index += 1;
+    }
+  }
+
+  add(member) {
+    if (this.team.has(member)) {
+      throw new Error('Такой персонаж уже есть в команде!');
+    }
+    return this.team.add(member);
+  }
+
+  addAll(...members) {
+    members.forEach((member) => this.team.add(member));
+    return this.team;
   }
 }
